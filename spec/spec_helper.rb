3db00a6fbf1a3003de99cca8aec4f12d5ef86f60
Ikhsan Assaat <ixnixnixn@yahoo.com>
2016-08-17 22:18:40 +0100

    Configure simplecov to only include lib

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pry'
+require 'simplecov'
 require 'coveralls'
 Coveralls.wear!
 
@@ -10,3 +11,8 @@ require 'swoop/project'
 require 'swoop/report'
 
 PROJECT_FIXTURE_PATH = 'spec/fixture/Swoop/Swoop.xcodeproj'
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter 'lib'
+end
