5e84ecc7f5c5bcdb27e8b00740a455355114cc33
Ikhsan Assaat <ixnixnixn@yahoo.com>
2016-08-18 18:09:31 +0100

    Test title

--- spec/swoop_spec.rb
+++ spec/swoop_spec.rb
@@ -9,29 +9,41 @@ end
 describe Swoop::Reporter do
   before do
     allow($stdout).to receive(:write)
-    allow_any_instance_of(described_class).to receive(:summary_report).and_return(report)
   end
 
   context "Renderer" do
     let(:report) { [ double(Swoop::Report) ] }
     let(:renderer) { double(Swoop::Renderer, { 'render' => '' }) }
 
+    before do
+      allow_any_instance_of(described_class).to receive(:summary_report).and_return(report)
+    end
+
+    it "should have correct title report" do
+      dir = "folder"
+      expected_title = "Swoop Report : '#{dir}'"
+      expect(Swoop::TableRenderer).to receive(:new).with(report, expected_title).and_return(renderer)
+
+      args = ["--path",  PROJECT_FIXTURE_PATH,  "--dir", dir]
+      described_class.start(args)
+    end
+
     it "should use TableRenderer when not specified" do
-      expect(Swoop::TableRenderer).to receive(:new).with(report, "Swoop Report : 'Classes'").and_return(renderer)
+      expect(Swoop::TableRenderer).to receive(:new).and_return(renderer)
 
       args = ["--path",  PROJECT_FIXTURE_PATH,  "--dir", "Classes"]
       described_class.start(args)
     end
 
     it "should use CSVRenderer for csv render option" do
-      expect(Swoop::CSVRenderer).to receive(:new).with(report, "Swoop Report : 'Classes'").and_return(renderer)
+      expect(Swoop::CSVRenderer).to receive(:new).and_return(renderer)
 
       args = ["--path",  PROJECT_FIXTURE_PATH,  "--dir", "Classes", "--render", "csv" ]
       described_class.start(args)
     end
 
     it "should use ChartRenderer for csv render option" do
-      expect(Swoop::ChartRenderer).to receive(:new).with(report, "Swoop Report : 'Classes'").and_return(renderer)
+      expect(Swoop::ChartRenderer).to receive(:new).and_return(renderer)
 
       args = ["--path",  PROJECT_FIXTURE_PATH,  "--dir", "Classes", "--render", "chart" ]
       described_class.start(args)
