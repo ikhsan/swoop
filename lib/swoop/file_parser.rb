4fae95daf539af68048af84ab9ff6dbc31434caf
Ikhsan Assaat <ixnixnixn@yahoo.com>
2017-02-26 01:23:41 +0000

    Introduce entity info

--- lib/swoop/file_parser.rb
+++ lib/swoop/file_parser.rb
@@ -2,23 +2,46 @@ require 'json'
 
 module Swoop
 
+  class EntityInfo
+
+    attr_reader :filepath, :line_count, :classes, :structs, :extensions
+
+    def initialize(filepath, line_count, classes, structs, extensions)
+      @filepath = filepath
+      @line_count = line_count
+      @classes = classes
+      @structs = structs
+      @extensions = extensions
+    end
+
+    def hash
+      filepath.hash
+    end
+
+    def eql?(comparee)
+      self == comparee
+    end
+
+    def ==(comparee)
+      self.filepath == comparee.filepath
+    end
+
+  end
+
   class FileParser
 
-    attr_reader :entities
+    attr_reader :entityInfos
 
     def self.parse(filepaths)
-      filepaths
-        .map { |p| self.new(p).entities }
-        .flatten
-        .uniq
+      filepaths.map { |p| self.new(p).entityInfos }.uniq
     end
 
     def initialize(filepath)
       @filepath = filepath
     end
 
-    def entities
-      @entitites ||= begin
+    def entityInfos
+      @entityInfos ||= begin
         return parse_swift if File.extname(@filepath) == ".swift"
         return parse_objc if File.extname(@filepath) == ".h"
         []
@@ -42,7 +65,7 @@ module Swoop
       structs = filter(SWIFT_STRUCT).map { |e| Entity.new(e, LANG_SWIFT, TYPE_STRUCT) }
       extensions = filter(SWIFT_EXT).map { |e| Entity.new(e, LANG_SWIFT, TYPE_EXTENSION) }
 
-      [ *classes, *structs, *extensions ]
+      EntityInfo.new(@filepath, file_content.lines.count, classes, structs, extensions)
     end
 
     OBJC_CLASS = '@interface\s*(\w+)\s*:'
@@ -59,7 +82,7 @@ module Swoop
       structs = (filter(OBJC_STRUCT, true, Regexp::MULTILINE) + filter(OBJC_STRUCT2))
         .map { |e| Entity.new(e, LANG_OBJC, TYPE_STRUCT) }
 
-      [ *classes, *structs, *categories ]
+      EntityInfo.new(@filepath, file_content.lines.count, classes, structs, categories)
     end
 
     def file_content
--- spec/file_parser_spec.rb
+++ spec/file_parser_spec.rb
@@ -8,36 +8,36 @@ describe Swoop::FileParser do
 
     it "should parse every class" do
       user = double(Swoop::Entity, { 'name' => 'User', 'language' => 'swift', 'type' => 'class' })
-      expect(subject.entities).to include(user)
+      expect(subject.entityInfos.classes).to include(user)
 
       admin = double(Swoop::Entity, { 'name' => 'Admin', 'language' => 'swift', 'type' => 'class' })
-      expect(subject.entities).to include(admin)
+      expect(subject.entityInfos.classes).to include(admin)
     end
 
     it "should parse every struct" do
       position = double(Swoop::Entity, { 'name' => 'Position', 'language' => 'swift', 'type' => 'struct' })
-      expect(subject.entities).to include(position)
+      expect(subject.entityInfos.structs).to include(position)
     end
 
     it "should parse every extension" do
       user = double(Swoop::Entity, { 'name' => 'User', 'language' => 'swift', 'type' => 'extension' })
-      expect(subject.entities).to include(user)
+      expect(subject.entityInfos.extensions).to include(user)
     end
 
     context "when commented" do
       it "should ignore commented classes" do
-        class_names = subject.entities.select { |e| e.type == 'class' }.map { |e| e.name }
+        class_names = subject.entityInfos.classes.map(&:name)
         expect(class_names).not_to include("_User")
         expect(class_names).not_to include("_Admin")
       end
 
       it "should ignore commented structs" do
-        struct_names = subject.entities.select { |e| e.type == 'struct' }.map { |e| e.name }
+        struct_names = subject.entityInfos.structs.map(&:name)
         expect(struct_names).not_to include("_Position")
       end
 
       it "should ignore commented extension" do
-        ext_names = subject.entities.select { |e| e.type == 'extension' }.map { |e| e.name }
+        ext_names = subject.entityInfos.extensions.map(&:name)
         expect(ext_names).not_to include("_User")
       end
     end
@@ -48,49 +48,49 @@ describe Swoop::FileParser do
 
     it "should parse class " do
       vc = double(Swoop::Entity, { 'name' => 'ViewController', 'language' => 'objc', 'type' => 'class' })
-      expect(subject.entities).to include(vc)
+      expect(subject.entityInfos.classes).to include(vc)
 
       svc = double(Swoop::Entity, { 'name' => 'SpecialViewController', 'language' => 'objc', 'type' => 'class' })
-      expect(subject.entities).to include(svc)
+      expect(subject.entityInfos.classes).to include(svc)
     end
 
     it "should parse categories" do
       cat = double(Swoop::Entity, { 'name' => 'SpecialViewController+Extension', 'language' => 'objc', 'type' => 'category' })
-      expect(subject.entities).to include(cat)
+      expect(subject.entityInfos.extensions).to include(cat)
     end
 
     it "should parse extension" do
       ext = double(Swoop::Entity, { 'name' => 'SpecialViewController', 'language' => 'objc', 'type' => 'extension' })
-      expect(subject.entities).to include(ext)
+      expect(subject.entityInfos.extensions).to include(ext)
     end
 
     it "should parse structs" do
       struct1 = double(Swoop::Entity, { 'name' => 'Color', 'language' => 'objc', 'type' => 'struct' })
-      expect(subject.entities).to include(struct1)
+      expect(subject.entityInfos.structs).to include(struct1)
 
       struct2 = double(Swoop::Entity, { 'name' => 'Coordinate', 'language' => 'objc', 'type' => 'struct' })
-      expect(subject.entities).to include(struct2)
+      expect(subject.entityInfos.structs).to include(struct2)
 
       struct3 = double(Swoop::Entity, { 'name' => 'Book', 'language' => 'objc', 'type' => 'struct' })
-      expect(subject.entities).to include(struct3)
+      expect(subject.entityInfos.structs).to include(struct3)
     end
 
     context "when commented" do
       it "should ignore commented classes" do
-        class_names = subject.entities.select { |e| e.type == 'class' }.map { |e| e.name }
+        class_names = subject.entityInfos.classes.map(&:name)
         expect(class_names).not_to include("_ViewController")
         expect(class_names).not_to include("_SpecialViewController")
       end
 
-      it "should ignore commented categories" do
-        ext_names = subject.entities.select { |e| e.type == 'extension' }.map { |e| e.name }
-        expect(ext_names).not_to include("_SpecialViewController")
-      end
-
       it "should ignore commented structs" do
-        struct_names = subject.entities.select { |e| e.type == 'struct' }.map { |e| e.name }
+        struct_names = subject.entityInfos.structs.map(&:name)
         expect(struct_names).not_to include("_Book")
         expect(struct_names).not_to include("_Cooordinate")
+      end
+
+      it "should ignore commented categories" do
+        ext_names = subject.entityInfos.extensions.map(&:name)
+        expect(ext_names).not_to include("_SpecialViewController")
       end
     end
   end
@@ -102,8 +102,8 @@ describe Swoop::FileParser do
     ]}
 
     it "should parse all entities without duplicates" do
-      entities = described_class.parse(filepaths)
-      dupes = entities.select { |e| entities.count(e) > 1 }
+      entityInfoss = described_class.parse(filepaths)
+      dupes = entityInfoss.select { |e| entityInfoss.count(e) > 1 }
 
       expect(dupes).to be_empty
     end
